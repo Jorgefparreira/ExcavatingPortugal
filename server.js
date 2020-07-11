b31bc214e850eea583cb440db30098aaac2cf2d7
Jorge <jorgefparreira@gmail.com>
2020-07-11 18:54:56 +0100

    package.json scripts update

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const path = require("path");
 const app = express(); 
 const port = process.env.PORT || "8000";
-const environment = process.env.NODE_ENV || 'development';
+const environment = process.env.PORT || 'development';
 let mongoUrl = '';
 let mapsCredential = '';
 if(environment === "development"){
