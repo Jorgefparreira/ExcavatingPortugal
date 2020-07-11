4053a23e3d7bf6977528c7fdd98a699d26be5b62
Jorge <jorgefparreira@gmail.com>
2020-07-11 18:15:43 +0100

    added dig detail and coming soon

--- index.js
+++ index.js
@@ -1,20 +1,48 @@
 const express = require("express");
 const path = require("path");
 const data = require("./public/js/data.json");
+const mongoUrl = require("./config.js").mongoUrl;
+const MongoClient = require('mongodb').MongoClient;
+const mapsCredential = require('./config.js').mapsCredential;
 
-const app = express();
+const app = express(); 
 const port = process.env.PORT || "8000";
+const environment = process.env.NODE_ENV || 'development';
+console.log(environment)
 
 app.set("views", path.join(__dirname, "views"));
 app.set('view engine', 'ejs')
 app.use(express.static(path.join(__dirname, "public")));
 
 app.get('/', function (req, res) {
-  res.render('../src/views/pages/index', { data })
+  MongoClient.connect(mongoUrl,{ useUnifiedTopology: true }, function(err, db) {
+    if (err) throw err;
+    var dbo = db.db("excavating_portugal");
+    dbo.collection("projects").find({}).sort( { id: 1 } ).toArray(function(err, data) {
+      if (err) throw err;
+      res.render('../public/views/pages/index', { data })
+      db.close();
+    });
+  });    
+  
 })
 
-app.get('/project', function (req, res) {
-  res.render('../src/views/pages/index', { title: 'Hey', message: 'Project!' })
+app.get('/:id', function(req, res){
+  MongoClient.connect(mongoUrl,{ useUnifiedTopology: true }, function(err, db) {
+    if (err) throw err;
+    var dbo = db.db("excavating_portugal");
+    dbo.collection("projects").find({"anchor":req.params.id }).toArray(function(err, result) {
+      if (err) throw err;
+      if(result[0].text){
+        const data = result[0]
+        res.render('../public/views/pages/dig_detail', { data, req, mapsCredential })
+        db.close();
+      } else {
+        res.render('../public/views/pages/coming_soon', {})
+        db.close();
+      }
+    });
+  });  
 })
 
 app.listen(port, () => {
